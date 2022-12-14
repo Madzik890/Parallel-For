cfa569556be8dd51d33e37a2fbfebb1adef670ca
Marcin <Madzik890@hotmail.com>
2022-12-14 15:18:20 +0100

    Performance fix

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <chrono>
+#include <random>
 #include <vector>
 #include <execution>
 
 #include "../include/parallelloops.hpp"
 
-constexpr const auto VECTOR_SIZE = 1000000;
+constexpr const auto VECTOR_SIZE = 100000000;
+static std::vector<int> vector(VECTOR_SIZE);
 
-inline auto GenerateFunction = [](int &value) {
-    do
-    {
-        value = rand() + 1;        
-    }
-    while(value == 0); 
+inline auto GenerateFunction = [](int value) {
+    static thread_local std::mt19937 generator;
+    std::uniform_int_distribution<int> distribution(-100,100);
+    value = distribution(generator);
 };
 
 inline void generate() noexcept
-{
-    std::srand(unsigned(std::time(nullptr)));
-    std::vector<int> vector(VECTOR_SIZE);
+{     
     std::for_each(vector.begin(), vector.end(), GenerateFunction);    
 }
 
 inline void parallelGenerate() noexcept
-{
-    std::srand(unsigned(std::time(nullptr)));
-    std::vector<int> vector(VECTOR_SIZE);
-
-    Parallel::ForeachRestricted(vector.begin(), vector.end(), GenerateFunction, 3);
+{ 
+    Parallel::ForeachRestricted(vector.begin(), vector.end(), GenerateFunction);
 }
 
 inline void executionGenerate() noexcept
-{
-    std::srand(unsigned(std::time(nullptr)));
-    std::vector<int> vector(VECTOR_SIZE);
+{       
     std::for_each(std::execution::par, vector.begin(), vector.end(), GenerateFunction);     
 }
 
@@ -50,14 +43,14 @@ int main()
         const auto begin = std::chrono::steady_clock::now();
         parallelGenerate();
         const auto end = std::chrono::steady_clock::now();
-        std::cout << "Parallel thread generate: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " ms" << std::endl;
+        std::cout << "Parallel thread generate: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << std::endl;
     }
     
     {
         const auto begin = std::chrono::steady_clock::now();
         executionGenerate();
         const auto end = std::chrono::steady_clock::now();
-        std::cout << "Execution generate      : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " ms" << std::endl;    
+        std::cout << "Execution generate      : " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << std::endl;    
     }
 
     return 0;
